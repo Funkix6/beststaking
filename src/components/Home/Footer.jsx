a514277a7717528785ec7aec3da52962bf56bc14
Funkix6 <briot-lucas@outlook.fr>
2022-02-17 10:39:14 +0100

    Small improvements

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Footer = () => {
     return(
-        <h1>
-            Footer
-        </h1>
+        <div className=" bg-gray-900 bg-opacity-30">
+            <div className="max-w-2xl mx-auto text-white py-10">
+                <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+                    <p className="order-2 md:order-1 mt-8 md:mt-0"> Best Staking, 2022 </p>
+                    <div className="order-1 md:order-2">
+                        <span className="px-2">About us</span>
+                        <span className="px-2 border-l">Contact us</span>
+                        <span className="px-2 border-l">Privacy Policy</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
